1d8728f29d68d31244e5cbf66cdf2c732de59d22
munneth <mugill@ucdavis.edu>
2025-07-15 18:58:38 -0700

    implemented usestate and useeffect

--- app/(pages)/_components/taskCard.jsx
+++ app/(pages)/_components/taskCard.jsx
@@ -2,14 +2,14 @@ import styles from './taskCard.module.scss';
 import Link from 'next/link';
 
 export default function TaskCard({ props }) {
-    const { title, description, url, hidden, small } = props;
+    const { id, title, description, url, hidden, small } = props;
 
     if (hidden) {
         return null; // Don't render anything if hidden
     }
 
     return (
-        <div className={styles.card}>
+        <div className={`${styles.card} ${small ? styles.smallCard : ''}`}>
             <h3>{title}</h3>
             <p>{description}</p>
             <Link href={url}></Link>
--- app/(pages)/_components/taskCarousel.jsx
+++ app/(pages)/_components/taskCarousel.jsx
@@ -1,16 +1,52 @@
 import TaskCard from './taskCard';
 import styles from './taskCarousel.module.scss';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+function rotateCards(props) {
+    if (center){
+        return (
+            styles.smallCard
+        )
+    }
+}
+
+
+
+
 
 export default function TaskCarousel({ }) {
 
+    const data = ["1", "2", "3", "4", "5"];
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const carouselInfiniteScroll = () =>{
+        if (currentIndex === data.length - 1){
+            return setCurrentIndex(0);
+        }
+        return setCurrentIndex(currentIndex + 1);
+    }
+
+    useEffect(()=> {
+        const interval = setInterval(() => {carouselInfiniteScroll()}, 3000)
+        return () => clearInterval(interval);
+    })
+
+    
     return (
         <div className={styles.carousel}>
-            <button>Prev</button>
-            <TaskCard props={{ title: 'Task 1', description: 'Task 1 description', url: '/task_example', hidden: true, small: true }} />
-            <TaskCard props={{ title: 'Task 2', description: 'Task 2 description', url: '/task_example', hidden: false, small: true }} />
-            <TaskCard props={{ title: 'Task 3', description: 'Task 3 description', url: '/task_example', hidden: false, small: false }} />
-            <TaskCard props={{ title: 'Task 4', description: 'Task 4 description', url: '/task_example', hidden: false, small: true }} />
-            <TaskCard props={{ title: 'Task 5', description: 'Task 5 description', url: '/task_example', hidden: true, small: true}} />
+            <button
+                className={styles.prevButton}
+                onClick={() => {
+                    console.log('Prev');
+
+                }}
+            >Prev</button>
+            <TaskCard props={{ id: 1, title: 'Task 1', description: 'Task 1 description', url: '/task_example', hidden: true, small: true, center: false }} />
+            <TaskCard props={{ id: 2, title: 'Task 2', description: 'Task 2 description', url: '/task_example', hidden: false, small: true, center: false }} />
+            <TaskCard props={{ id: 3, title: 'Task 3', description: 'Task 3 description', url: '/task_example', hidden: false, small: false, center:true }} />
+            <TaskCard props={{ id: 4, title: 'Task 4', description: 'Task 4 description', url: '/task_example', hidden: false, small: true, center: false }} />
+            <TaskCard props={{ id: 5, title: 'Task 5', description: 'Task 5 description', url: '/task_example', hidden: true, small: true, center: false}} />
             <button>Next</button>
         </div>
     );
