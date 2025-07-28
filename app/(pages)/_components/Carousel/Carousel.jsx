8bbbb18a14ab6710150e6b2c8a054b45d44e7403
austin1im <alaustinall@gmail.com>
2025-07-27 19:37:16 -0700

    functional image slideshow
    made each panel display a "calculated" index each time instead of sliding between x values

--- app/(pages)/_components/Carousel/Carousel.jsx
+++ app/(pages)/_components/Carousel/Carousel.jsx
@@ -2,35 +2,50 @@
 import styles from "./Carousel.module.scss";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Carousel ({images}) {
-    const [current, setCurrent] = useState(0)
+    const [main, setMain] = useState(0)
+    const [left, setLeft] = useState(images.length - 1)
+    const [right, setRight] = useState(1)
+
+    useEffect(()=>{
+        if (main == 0) {
+            setLeft(images.length - 1)
+            setRight(1)
+        }
+        else if (main == images.length - 1) {
+            setRight(0)
+            setLeft(main - 1)
+        }
+        else {
+            setLeft(main - 1)
+            setRight(main + 1)
+        }
+    },[main])
+
     const prev = () => 
-        setCurrent((current) => (current == 0 ? images.length - 1 : current - 1))
+        setMain(main == 0 ? images.length - 1 : main - 1)
     const next = () => 
-        setCurrent((current) => (current == images.length - 1 ? 0 : current + 1))
+        setMain(main == images.length - 1 ? 0 : main + 1)
 
     return (
         <main>
             <h1 className={styles.title}>Carousel</h1>
             <div className={styles.sliderContainer}>
-                <div className={styles.leftArrow} onClick={prev}>
+                <div className={styles.sideArrow} onClick={prev}>
                     <FaAngleLeft size="5rem"/>
                 </div>
-                <div className={styles.leftPanel}>
+                <div className={styles.smallPanel}>
+                    <Image width={256} height={256} src={images[left]} alt="idk"/>
                 </div>
-                <div className={styles.middlePanel}>
-                    <div className={styles.imageList} style={{transform: `translateX(-${current * 100}%)`}}>
-                        {images.map((image, index) => (
-                            <Image key={index} width={512} height={512} src={image} alt="idk"/>
-                        ))}
-                    </div>
+                <div className={styles.bigPanel}>
+                    <Image width={512} height={512} src={images[main]} alt="idk"/>
                 </div>
-                <div className={styles.rightPanel}>
-                    bye
+                <div className={styles.smallPanel}>
+                    <Image width={256} height={256} src={images[right]} alt="idk"/>
                 </div>
-                <div className={styles.rightArrow} onClick={next}>
+                <div className={styles.sideArrow} onClick={next}>
                     <FaAngleRight size="5rem"/>
                 </div>
             </div>
