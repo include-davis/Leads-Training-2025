70d921066969d184b54a40ff23bc70ffed55cd0b
austin1im <alaustinall@gmail.com>
2025-07-27 18:52:15 -0700

    testing single panel carousel

--- app/(pages)/_components/Carousel/Carousel.jsx
+++ app/(pages)/_components/Carousel/Carousel.jsx
@@ -1,25 +1,36 @@
+"use client";
 import styles from "./Carousel.module.scss";
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import Image from "next/image";
+import { useState } from "react";
 
+export default function Carousel ({images}) {
+    const [current, setCurrent] = useState(0)
+    const prev = () => 
+        setCurrent((current) => (current == 0 ? images.length - 1 : current - 1))
+    const next = () => 
+        setCurrent((current) => (current == images.length - 1 ? 0 : current + 1))
 
-export default function Carousel () {
     return (
         <main>
             <h1 className={styles.title}>Carousel</h1>
             <div className={styles.sliderContainer}>
-                <div className={styles.leftArrow}>
+                <div className={styles.leftArrow} onClick={prev}>
                     <FaAngleLeft size="5rem"/>
                 </div>
                 <div className={styles.leftPanel}>
-                    hi
                 </div>
                 <div className={styles.middlePanel}>
-                    hello
+                    <div className={styles.imageList} style={{transform: `translateX(-${current * 100}%)`}}>
+                        {images.map((image, index) => (
+                            <Image key={index} width={512} height={512} src={image} alt="idk"/>
+                        ))}
+                    </div>
                 </div>
                 <div className={styles.rightPanel}>
                     bye
                 </div>
-                <div className={styles.rightArrow}>
+                <div className={styles.rightArrow} onClick={next}>
                     <FaAngleRight size="5rem"/>
                 </div>
             </div>
--- app/(pages)/task2/page.jsx
+++ app/(pages)/task2/page.jsx
@@ -1,11 +1,19 @@
 import styles from './task2.module.scss';
 import Carousel from '../_components/Carousel/Carousel';
 
+const images = [
+  "/cute.png",
+  "/baseball.png",
+  "/beast.png",
+  "/cookie.png",
+  "/tears.png"
+]
+
 export default function Task() {
   return (
     <main>
       <div className={styles.mainContainer}>
-        <Carousel/>
+        <Carousel images={images}/>
       </div>
     </main>
   );
