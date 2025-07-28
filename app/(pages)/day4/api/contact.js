2ba497dc223d45ec9d0f7f06da9d9a19d8813621
Theresadinh <thexresa04@gmail.com>
2025-07-27 18:28:55 -0700

    Update

--- app/(pages)/day4/api/contact.js
+++ app/(pages)/day4/api/contact.js
@@ -1,8 +1,52 @@
-// import nodemailer from "nodemailer";
 
-// const senderEmail = process.env.SENDER_EMAIL;
-// const password = process.env.SENDER_PWD;
-// const targetEmail = process.env.TARGET_EMAIL;
+import nodemailer from "nodemailer";
 
-// const nodemailer = require('nodemailer');
+const senderEmail = process.env.SENDER_EMAIL;
+const password = process.env.SENDER_PWD;
+const targetEmail = process.env.TARGET_EMAIL;
 
+export default async function contactFormMailer(req, res) {
+  
+    console.log("EMAIL ENV:", senderEmail, password, targetEmail);
+    console.log("REQUEST BODY:", req.body);
+
+  
+    if (req.method !== "POST") {
+    return res.status(405).json({ ok: false, error: "Method Not Allowed" });
+  }
+
+    try {
+      const { name, email, message } = req.body;
+
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: senderEmail,
+            pass: password,
+        }
+        });
+
+        const msg = `
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+        `;
+
+      const mailOptions = {
+        from: `${name} <${senderEmail}> `,
+        to: targetEmail,
+        subject: `Contact Form - ${name} has a message: ${message.substr(0, 30)}...`,
+        replyTo: email,
+        msg,
+      };
+
+    await transporter.sendMail(mailOptions);
+
+    return res.status(200).json({ ok: true });
+  } catch (e) {
+    console.error("Email error:", e);
+    return res.status(500).json({ ok: false, error: e.message });
+  }
+}
--- app/(pages)/day4/page.jsx
+++ app/(pages)/day4/page.jsx
@@ -1,27 +1,59 @@
-import styles from './page.module.scss';
 
+'use client';
+import React, { useState, useRef } from "react";
+import styles from './page.module.scss';
+import Image from "next/image";
 
 export default function ContactForm() {
+
+  const [form, setForm] = useState({name: '', email: '', message: ''});
+  const [status, setStatus] = useState('');
+
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      const resData = await res.json();
+      if (!res.ok) {
+        throw new Error(resData.error || "Unknown error");
+      }
+      setStatus("Success");
+    } catch (e) {
+      setStatus("Failed");
+    }
+  };
   return (
     
     <div className={styles.formBox}>
 
-      <form id="contactForm" className={styles.contactForm}> 
+      <form id="contactForm" className={styles.contactForm} onSubmit={handleSubmit}> 
         <h1> Contact Form </h1>
         
         <label htmlFor="name">Name:</label>
-        <input className={styles.input} type="text" id="name" name="name" required />
+        <input className={styles.input} type="text" id="name" name="name" onChange={handleChange} required />
 
         <label htmlFor="email">Email:</label>
-        <input className={styles.input} type="email" id="email" name="email" required />
+        <input className={styles.input} type="email" id="email" name="email" onChange={handleChange} required />
 
         <label htmlFor="message">Message:</label>
-        <textarea className={styles.textarea} id="message" name="message" required></textarea>
+        <textarea className={styles.textarea} id="message" name="message" onChange={handleChange}></textarea>
 
         <button className={styles.submit} type="submit">Submit</button>
       </form>
-      <div id="statusMessage"></div>
 
+      <div id="statusMessage">{status}</div>
 
     </div>
   );
