1b3b406cf547f25631eb39d0f1cdc600ecd842c4
austin1im <alaustinall@gmail.com>
2025-07-29 00:54:48 -0700

    data stores in a usestate

--- app/(pages)/task4/page.jsx
+++ app/(pages)/task4/page.jsx
@@ -3,15 +3,20 @@ import styles from './task4.module.scss';
 import { useState } from 'react';
 
 
-export default function Task() {
+export default function ContactForm() {
+  const [form, updateForm] = useState({name: '', email: '', message: ''})
+
+  const handleChange = (e) => {
+    updateForm({...form, [e.target.name]: e.target.value})
+  }
   return (
     <main>
       <div className={styles.mainContainer}>
         Contact Form
-        <form className={styles.formContainer}>
-          <input name="name" placeholder='name'/>
-          <input name="email" placeholder='email'/>
-          <textarea name="message" placeholder='message'/>
+        <form className={styles.formContainer} onSubmit={handleSubmit}>
+          <input name="name" placeholder='name' onChange={handleChange}/>
+          <input name="email" placeholder='email' onChange={handleChange}/>
+          <textarea name="message" placeholder='message' onChange={handleChange}/>
           <button type='submit'>submit</button>
         </form>
       </div>
